6761afb156215737d8cbf8f83fc5017188db0abc
Melih <turkmenmelih01@gmail.com>
2023-05-23 14:11:25 +0300

    asp.net.core identity project

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -0,0 +1,85 @@
+﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        //ıdentity kütüphanesinden gelen her özelliği kullanmamak adına appuserregister kullanılıyor 
+        public async Task<IActionResult> Index(AppUserRegisterDto appUserRegisterDto)
+        {
+            if (ModelState.IsValid)
+            {
+                //random olarak confirmcode üretiliyor
+                Random random = new Random();
+                int code;
+                code = random.Next(100000, 1000000);
+                AppUser appUser = new AppUser()
+                {
+                    UserName = appUserRegisterDto.Username,
+                    Name = appUserRegisterDto.Name,
+                    Surname = appUserRegisterDto.Surname,
+                    Email = appUserRegisterDto.Email,
+                    City = "aaaa",
+                    District = "bbbb",
+                    ImageUrl = "cccc",
+                    ConfirmCode = code
+                };
+                
+                var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
+                if (result.Succeeded)
+                {
+                    //mail gönderme işlemi yapılıyor 
+                    MimeMessage mimeMessage = new MimeMessage();
+                    MailboxAddress mailboxAddressFrom = new MailboxAddress("Easy Cash Admin", "turkmenmelih01@gmail.com"); //maili kim gönderiyor
+                    MailboxAddress mailboxAddressTo = new MailboxAddress("User", appUser.Email); //appuserdan gelen mail adresine kod gönderiliyor
+
+                    mimeMessage.From.Add(mailboxAddressFrom);
+                    mimeMessage.To.Add(mailboxAddressTo);
+
+                    var bodyBuilder = new BodyBuilder(); //mail içeriğini oluşturuyoruz 
+                    bodyBuilder.TextBody = "Kayıt işlemini gerçekleştirmek için onay kodunuz:" + code;
+                    mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+                    mimeMessage.Subject = "Easy Cash Onay Kodu";
+
+                    SmtpClient client = new SmtpClient();
+                    client.Connect("smtp.gmail.com", 587, false); //protocol sağlanıyor 
+                    client.Authenticate("turkmenmelih01@gmail.com", "liaieujafylbhahb"); //uygulama şifresi 2 adımlı doğrulamanın aktif olması gereklidir
+                    client.Send(mimeMessage);
+                    client.Disconnect(true);
+
+                    TempData["Mail"] = appUserRegisterDto.Email;
+
+                    return RedirectToAction("Index", "ConfirmMail");
+                }
+                else
+                {
+                    foreach(var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View();
+        }
+        //şifre en az 6 karakter olmalıdır,özel karakter olmalı,büyük küçük harf olmalı
+    }
+}
---
+++ EasyCashIdentityProject.PresentationLayer/Models/CustomIdentityValidator.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EasyCashIdentityProject.PresentationLayer.Models
+{
+    public class CustomIdentityValidator : IdentityErrorDescriber //hataları türkçe olmasını sağlar  register işlemleri
+                                                                                                     
+    {               
+        //overrride methodun işleyişini bozmaz ama bizim istediğimiz formatta çalışır 
+        //passwordtoshort şifrenin çok kısa olduğunu döner 
+        public override IdentityError PasswordTooShort(int length)              
+        {
+            return new IdentityError()                                           
+            {
+                Code = "PasswordTooShort",
+                Description = $"Parola en az {length} karakter olmalıdır" //$ işareti varsa mutlaka parametre geçilmelidir
+            };
+        }
+        //passwordrequiresuppre şifrede en az 1 büyük harf ister 
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresUpper",
+                Description = "Lütfen en az 1 büyük harf giriniz"
+            };
+        }
+        //passwordrequireslower şifrede en az 1 küçük harf ister 
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresLower",
+                Description = "Lütfen en az 1 küçük harf giriniz"
+            };
+        }
+        //passwordrequiredigit en az 1 tane rakam ister 
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresDigit",
+                Description = "Lütfen en az 1 tane rakam giriniz"
+            };
+        }
+        //passwordrequiresnonalphanumeric en az 1 tane sembol ister 
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresNonAlphanumeric",
+                Description = "Lütfen en az 1 tane sembol giriniz"
+            };
+        }
+    }
+}
